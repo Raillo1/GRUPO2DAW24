655c9a5bbf20ddf46c05a8901f10e5d74e3ab028
David Bejarano Llano <sonicorn.db@gmail.com>
2024-11-21 14:35:01 +0100

    Config.php con la información de nuestra base de datos

--- app/Config.php
+++ app/Config.php
@@ -2,10 +2,10 @@
 
  class Config
  {
-     static public $mvc_bd_hostname = "localhost";
-     static public $mvc_bd_nombre   = "alimentosalex";
-     static public $mvc_bd_usuario  = "myale7993";
-     static public $mvc_bd_clave    = "23mar2005";
-     static public $mvc_vis_css     = "estilo.css";
+     static public $mvc_bd_hostname = "Servidor:gregoriobs9902.es";
+     static public $mvc_bd_nombre   = "alimentos";
+     static public $mvc_bd_usuario  = "mygregorio40";
+     static public $mvc_bd_clave    = "A9Jwm28i";
+     static public $mvc_vis_css     = "";
  }
 ?>
