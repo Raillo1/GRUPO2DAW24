2290a8addd8ff41e11ce675be9212471e4fd3d73
David Bejarano Llano <sonicorn.db@gmail.com>
2024-11-28 13:51:55 +0100

    Subida de archivos Config, controller e index

---
+++ app/Config.php
@@ -0,0 +1,11 @@
+<?php
+
+class Config
+{
+    static public $mvc_bd_hostname = "gregoriobs9902.es";
+    static public $mvc_bd_nombre   = "alimentos22024";
+    static public $mvc_bd_usuario  = "mygregorio40";
+    static public $mvc_bd_clave    = "A9Jwm28i";
+    static public $mvc_vis_css     = "";
+}
+?>
